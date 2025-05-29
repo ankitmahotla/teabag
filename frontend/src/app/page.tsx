0311789bd4dddd6e4c70c743f2acea16fbc23f3d
ankitmahotla <ankitmahotla45@gmail.com>
2025-05-29 09:59:24 +0530

    feat(theme): add theme & theme toggle

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 export default function Home() {
-  return <div>hi teabag</div>;
+  return (
+    <div className="max-w-7xl mx-auto flex items-start justify-between p-2">
+      <h1 className="text-2xl font-medium">Teabag</h1>
+      <ModeToggle />
+    </div>
+  );
 }
