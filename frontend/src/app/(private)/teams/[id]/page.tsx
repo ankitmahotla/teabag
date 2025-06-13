6b1cb5202724cf37c3574d991c5ed1094814688a
ankitmahotla <ankitmahotla45@gmail.com>
2025-06-13 21:08:23 +0530

    feat(teams): wrap team components in ErrorBoundary for improved error handling

--- frontend/src/app/(private)/teams/[id]/page.tsx
+++ frontend/src/app/(private)/teams/[id]/page.tsx
@@ -3,11 +3,14 @@
 import { Suspense } from "react";
 import { Spinner } from "@/components/spinner";
 import { Team } from "@/components/team/team";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default function Page() {
   return (
     <Suspense fallback={<Spinner />}>
-      <Team />
+      <ErrorBoundary fallback={<div>Team not found</div>}>
+        <Team />
+      </ErrorBoundary>
     </Suspense>
   );
 }
--- frontend/src/app/(private)/teams/page.tsx
+++ frontend/src/app/(private)/teams/page.tsx
@@ -3,11 +3,14 @@
 import { Suspense } from "react";
 import { Spinner } from "@/components/spinner";
 import { TeamsCatalog } from "@/components/team/teams-catalog";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default function Page() {
   return (
     <Suspense fallback={<Spinner />}>
-      <TeamsCatalog />
+      <ErrorBoundary fallback={<div>Error fetching teams</div>}>
+        <TeamsCatalog />
+      </ErrorBoundary>
     </Suspense>
   );
 }
